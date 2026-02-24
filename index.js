4518d58a1914d1e098ada00bf33b57ceec838678
shin <z5483669@ad.unsw.edu.au>
2026-02-25 01:40:03 +1100

    initialise bot with basic echo command

---
+++ index.js
@@ -0,0 +1,23 @@
+import dotenv from 'dotenv'
+dotenv.config()
+
+import { Client, GatewayIntentBits } from 'discord.js'
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.DirectMessages,
+    GatewayIntentBits.MessageContent
+  ],
+});
+
+client.login(process.env.DISCORD_TOKEN);
+
+client.on("messageCreate", async (message) => {
+  
+  if (message.author.bot) return;
+
+  message.channel.send(`${message.content}`);
+});
