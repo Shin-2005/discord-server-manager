7a41fe240cbbf8468f4e7f4ddc0c83844db2cc0d
Wesley Chan <wesley.chan2@students.mq.edu.au>
2026-03-06 19:46:56 +1100

    Added race as input for start, and also starting to add MongoDB

--- commands/utility/start.js
+++ commands/utility/start.js
@@ -10,15 +10,14 @@ export default {
     .setCustomId('createCharacter')
     .setTitle('Create Your Character');
 
+    // Name Input
+
     const nameInput = new TextInputBuilder()
     .setCustomId('charName')
     .setStyle(TextInputStyle.Short)
     .setRequired(true);
 
-    const nameLabel = new LabelBuilder()
-    .setLabel('Name')
-    .setTextInputComponent(nameInput);
-    
+    // Class Input
 
    const classInput = new StringSelectMenuBuilder()
     .setCustomId('charClass')
@@ -31,13 +30,41 @@ export default {
       new StringSelectMenuOptionBuilder().setLabel('Bard').setValue('bard'),
     );
 
+    // Race Input
+       const raceInput = new StringSelectMenuBuilder()
+    .setCustomId('charRace')
+    .setPlaceholder('Select your race')
+    .setRequired(true)
+    .addOptions(
+      new StringSelectMenuOptionBuilder().setLabel('Human').setValue('human'),
+      new StringSelectMenuOptionBuilder().setLabel('Elf').setValue('elf'),
+      new StringSelectMenuOptionBuilder().setLabel('Dwarf').setValue('dwarf'),
+      new StringSelectMenuOptionBuilder().setLabel('Orc').setValue('orc'),
+       new StringSelectMenuOptionBuilder().setLabel('Halfling').setValue('halfling'),
+      new StringSelectMenuOptionBuilder().setLabel('Dragonborn').setValue('dragonborn'),
+      
+    );
+
+    // Label Builders
+
+    const nameLabel = new LabelBuilder()
+    .setLabel('Name')
+    .setTextInputComponent(nameInput);
+    
+
     const classLabel = new LabelBuilder()
     .setLabel('Class')
     .setStringSelectMenuComponent(classInput);
 
-    modal.addLabelComponents(nameLabel, classLabel);
+    const raceLabel = new LabelBuilder()
+    .setLabel('Race')
+    .setStringSelectMenuComponent(raceInput);
+
+    modal.addLabelComponents(nameLabel, classLabel, raceLabel);
 
     await interaction.showModal(modal);
 
   },
+
+  
 };
